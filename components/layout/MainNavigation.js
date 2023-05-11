4c4c9fcd4878a9d9323a1bd9da178dc929413b4a
Tushar Kumar Verma <tushar.workplace@gmail.com>
2023-05-11 19:35:38 +0530

    Update MainNavigation.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -5,7 +5,7 @@ import classes from './MainNavigation.module.css'
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>Meetups</div>
       <nav>
         <ul>
           <li>
